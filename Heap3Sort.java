b6e84be1a32afcd703d501826b2f3e494fdb38ac
MrEntity303 <patryk.bialowas@studenti.unicam.it>
2022-07-13 14:30:46 +0200

    [IMPLEMENTED] Heap3Sort.java

--- Heap3Sort.java
+++ Heap3Sort.java
@@ -10,31 +10,59 @@ import java.util.List;
  * ternario. Usa una variante dei metodi di un MaxHeap ternario in modo da
  * implementare l'algoritmo utilizzando solo un array (arraylist) e alcune
  * variabili locali di appoggio (implementazione cosiddetta "in loco" o "in
- * place", si veda https://it.wikipedia.org/wiki/Algoritmo_in_loco)
- * 
+ * place", si veda <a href="https://it.wikipedia.org/wiki/Algoritmo_in_loco">...</a>)
+ *
  * Uno heap ternario è uno heap in cui ogni nodo ha tre figli e non due, come in
  * uno heap binario. La strategia di rappresentazione e i metodi d'inserimento
  * / estrazione del minimo / heapify devono essere adattati al caso di tre
  * figli, ma algoritmica-mente sono analoghi.
- * 
+ *
  * Lo heap ternario deve essere pensato in modo che accetti elementi ripetuti e
  * non accetti elementi null.
- * 
+ *
  * @author Luca Tesei (Template)
  *
  */
 public class Heap3Sort<E extends Comparable<E>> implements SortingAlgorithm<E> {
 
-    //TODO inserire eventuali variabili istanza che servono
+    private int dim;
+    private int count;
+
 
     @Override
     public SortingAlgorithmResult<E> sort(List<E> l) {
-        //TODO implementare
-        return null;
+        dim = l.size();
+        for (int i = l.size() / 3; i >= 0; i--)
+            heapify(l, i);
+
+        for(dim=l.size()-1;dim>=0;dim--){
+            swap(l, 0, dim);
+            heapify(l,0);
+        }
+
+        return new SortingAlgorithmResult<>(l, count);
     }
 
     private void heapify(List<E> l, int i) {
-        //TODO implementare adattando al caso di heap ternario
+        int max = i;
+        if(this.getFirstChildNode(i) < dim && l.get(this.getFirstChildNode(i)).compareTo(l.get(max)) >= 0)
+        {
+            max = this.getFirstChildNode(i);
+        }
+        if(this.getSecondChildNode(i) < dim && l.get(this.getSecondChildNode(i)).compareTo(l.get(max)) >= 0)
+        {
+            max = this.getSecondChildNode(i);
+        }
+        if(this.getThirdChildNode(i) < dim && l.get(this.getThirdChildNode(i)).compareTo(l.get(max)) >= 0)
+        {
+            max = this.getThirdChildNode(i);
+        }
+        if(max != i)
+        {
+            swap(l, i, max);
+            heapify(l, max);
+            count++;
+        }
     }
 
     @Override
@@ -48,5 +76,26 @@ public class Heap3Sort<E extends Comparable<E>> implements SortingAlgorithm<E> {
      * che la posizione 0 è significativa e contiene sempre la radice dello
      * heap.
      */
+    private int getFirstChildNode(int pos)
+    {
+        return (3*pos) + 1;
+    }
+
+    private int getSecondChildNode(int pos)
+    {
+        return ((3*pos) + 1)+1;
+    }
+
+    private int getThirdChildNode(int pos)
+    {
+        return ((3*pos) + 1)+2;
+    }
+
+    private void swap(List<E> l, int i, int j)
+    {
+        E element = l.get(i); //posizione i-esima
+        l.set(i, l.get(j)); //sostituisce elemento gli elementi
+        l.set(j, element); //esegue il set dell'elemento di appoggio
+    }
 
 }
