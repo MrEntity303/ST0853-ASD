83e191489da7c4e96200eeedcefdb716e6b576bd
Simone Cisca <simone.cisca@studenti.unicam.it>
2022-07-14 20:04:48 +0200

    [Fixed] fixed getShortestPathTo(GraphNode<L> targetNode) in BellmanFordShortestPathComputer.java

--- BellmanFordShortestPathComputer.java
+++ BellmanFordShortestPathComputer.java
@@ -1,5 +1,7 @@
 package it.unicam.cs.asdl2122.pt1;
 
+import sun.security.x509.EDIPartyName;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,11 +14,9 @@ import java.util.Set;
  * Implementazione dell'algoritmo di Bellman-Ford per il calcolo di cammini
  * minimi a sorgente singola in un graph pesato che può contenere anche pesi
  * negativi, ma non cicli di peso negativo.
- * 
- * @author Template: Luca Tesei
  *
- * @param <L>
- *                etichette dei nodi del graph
+ * @param <L> etichette dei nodi del graph
+ * @author Template: Luca Tesei
  */
 public class BellmanFordShortestPathComputer<L>
         implements SingleSourceShortestPathComputer<L> {
@@ -31,31 +31,23 @@ public class BellmanFordShortestPathComputer<L>
      * Crea un calcolatore di cammini minimi a sorgente singola per un graph
      * orientato e pesato.
      *
-     * @param graph
-     *                  il graph su cui opera il calcolatore di cammini minimi
-     * @throws NullPointerException
-     *                                      se il graph passato è nullo
-     *
-     * @throws IllegalArgumentException
-     *                                      se il graph passato è vuoto
-     *
-     * @throws IllegalArgumentException
-     *                                      se il graph passato non è diretto
-     *
-     * @throws IllegalArgumentException
-     *                                      se il graph passato non è pesato,
-     *                                      cioè esiste almeno un arco il cui
-     *                                      peso è {@code Double.NaN}.
+     * @param graph il graph su cui opera il calcolatore di cammini minimi
+     * @throws NullPointerException     se il graph passato è nullo
+     * @throws IllegalArgumentException se il graph passato è vuoto
+     * @throws IllegalArgumentException se il graph passato non è diretto
+     * @throws IllegalArgumentException se il graph passato non è pesato,
+     *                                  cioè esiste almeno un arco il cui
+     *                                  peso è {@code Double.NaN}.
      */
     public BellmanFordShortestPathComputer(Graph<L> graph) {
-        if(graph == null)
+        if (graph == null)
             throw new NullPointerException("Il grafo passato e' nullo.");
-        if(graph.isEmpty())
+        if (graph.isEmpty())
             throw new IllegalArgumentException("Il grafo passato e' vuoto.");
-        if(!graph.isDirected())
+        if (!graph.isDirected())
             throw new IllegalArgumentException("Il grafo passato non e' diretto.");
         for (GraphEdge<L> newEdge : graph.getEdges()) {
-            if(!newEdge.hasWeight())
+            if (!newEdge.hasWeight())
                 throw new IllegalArgumentException("Il grafo passato non e' pesato.");
         }
         this.graph = graph;
@@ -65,33 +57,31 @@ public class BellmanFordShortestPathComputer<L>
     }
 
     @Override
-    public void computeShortestPathsFrom(GraphNode<L> sourceNode)
-    {
-        if(sourceNode == null)
+    public void computeShortestPathsFrom(GraphNode<L> sourceNode) {
+        if (sourceNode == null)
             throw new NullPointerException("Il nodo passato e' nullo.");
 
-        if(!graph.getNodes().contains(sourceNode))
+        if (!graph.getNodes().contains(sourceNode))
             throw new IllegalArgumentException("Il nodo passato non esiste.");
 
         //step 1
         //confronto ogni nodo del grafo con un nuovo nodo
-        //source sarà il nodo da scoprire
-        for (GraphNode<L> source : this.getGraph().getNodes())
-        {
+        //next sarà il nodo da scoprire
+        for (GraphNode<L> next : this.getGraph().getNodes()) {
             //il nuovo nodo sorgente avrà distanza pari a infinito
-            source.setFloatingPointDistance(Double.POSITIVE_INFINITY);
+            next.setFloatingPointDistance(Double.POSITIVE_INFINITY);
+            next.setPrevious(null);
             //se il nodo passato è uguale a quello da confrontare
             //vuol dire che quello sarà a prescindere il nodo
             //di partenza da cui inizia a calcolare il
             //cammino minimo, quindi lo setto a 0.
-            if(sourceNode.equals(source))
-                sourceNode.setFloatingPointDistance(0.0);
+            if (sourceNode.equals(next))
+                next.setFloatingPointDistance(0.0);
         }
 
         //step2
         //scorro tutti i nodi del grafo
-        for (int i = 0; i < this.getGraph().nodeCount() - 1; i++)
-        {
+        for (int i = 0; i < this.getGraph().nodeCount() - 1; i++) {
             /*
              * per ogni arco del grafo creo due nodi:
              * uno sorgente (node1), e uno di destinazione (node2),
@@ -99,28 +89,25 @@ public class BellmanFordShortestPathComputer<L>
              * copio la distanza di node1 + il peso del suo arco
              * nella distanza di node2
              * */
-            for (GraphEdge<L> newEdge : graph.getEdges())
-            {
-                GraphNode<L> node1 = newEdge.getNode1();
-                GraphNode<L> node2 = newEdge.getNode2();
-                if (node2.getFloatingPointDistance() > node1.getFloatingPointDistance()
-                        + newEdge.getWeight())
-                {
-                    node2.setFloatingPointDistance(node1.getFloatingPointDistance()
+            for (GraphEdge<L> newEdge : this.graph.getEdges()) {
+                if (newEdge.getNode2().getFloatingPointDistance() > newEdge.getNode1().getFloatingPointDistance()
+                        + newEdge.getWeight()) {
+                    newEdge.getNode2().setFloatingPointDistance(newEdge.getNode1().getFloatingPointDistance()
                             + newEdge.getWeight());
+                    newEdge.getNode2().setPrevious(newEdge.getNode1());
                 }
             }
         }
         //step 3
         /*
-             * per ogni arco del grafo creo due nodi:
-             * uno sorgente (node1), e uno di destinazione (node2),
-             * se la distanza di node1 + il peso del suo nuovo arco
-             * e' minore della distanza di node2,
-             * ci sarà un ciclo con peso negativo
-             * */
-        for (GraphEdge<L> newEdge: this.getGraph().getEdges())
-            if((newEdge.getNode1().getFloatingPointDistance() + newEdge.getWeight()) < newEdge.getNode2().getFloatingPointDistance())
+         * per ogni arco del grafo creo due nodi:
+         * uno sorgente (node1), e uno di destinazione (node2),
+         * se la distanza di node1 + il peso del suo nuovo arco
+         * è minore della distanza di node2,
+         * ci sarà un ciclo con peso negativo
+         * */
+        for (GraphEdge<L> newEdge : this.getGraph().getEdges())
+            if ((newEdge.getNode1().getFloatingPointDistance() + newEdge.getWeight()) < newEdge.getNode2().getFloatingPointDistance())
                 throw new IllegalStateException("Il grafo presenta un ciclo con peso negativo.");
 
 
@@ -142,7 +129,7 @@ public class BellmanFordShortestPathComputer<L>
 
     @Override
     public GraphNode<L> getLastSource() {
-        if(lastSource == null)
+        if (lastSource == null)
             throw new IllegalStateException("Il calcolo non e' stato eseguito");
         return lastSource;
     }
@@ -155,36 +142,33 @@ public class BellmanFordShortestPathComputer<L>
     @Override
     public List<GraphEdge<L>> getShortestPathTo(GraphNode<L> targetNode) {
         // TODO implementare
-        if(targetNode == null)
+        if (targetNode == null)
             throw new NullPointerException("Il nodo passato è nullo.");
 
-        if(!this.graph.getNodes().contains(targetNode))
+        if (!this.graph.getNodes().contains(targetNode))
             throw new IllegalArgumentException("Il nodo passato non esiste.");
 
-        if(!isComputed())
+        if (!isComputed())
             throw new IllegalStateException("Il calcolo non e' stato eseguito.");
 
-        List<GraphEdge<L>> edgeList = new ArrayList<>();
+        List<GraphEdge<L>> toReturn = new ArrayList<>();
 
-        /*
         while (targetNode.getPrevious() != null) {
             // aggiungo l'arco che lo collega al precedente
-            System.out.println("Non c'è niente");
-            edgeList.add(graph.getEdge(targetNode.getPrevious(), targetNode));
-            if (targetNode.equals(this.getLastSource()))
+            // System.out.println("Non c'è niente");
+            toReturn.add(graph.getEdge(targetNode.getPrevious(), targetNode));
+
+            if (!targetNode.equals(this.getLastSource()))
+                // il targetNode diventa il suo previous e continuo finché non ne ha più
+                targetNode = targetNode.getPrevious();
+            else
                 break;
-
-            // il targetNode diventa il suo previous e continuo finché non ne ha più
-            targetNode = targetNode.getPrevious();
         }
-*/
+//Dato che il target si trova alla fine e inizio a scorrere da li,
+//faccio il reverse dell'array per avere gli archi ordinati dal primo all'ultimo
 
-       // Set<GraphNode<L>> toRetrun = graph.getNodes();
-
-//          Dato che il target si trova alla fine e inizio a scorrere da li,
-//          faccio il reverse dell'array per avere gli archi ordinati dal primo all'ultimo
-        Collections.reverse(edgeList);
-        return edgeList;
+        Collections.reverse(toReturn);
+        return toReturn;
     }
     // TODO inserire eventuali metodi privati per l'implementazione
 }
